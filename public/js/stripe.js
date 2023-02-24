b26f532ec5a79416766fc450767ae9fa895c2887
1amSumit <sj779619@gmail.com>
2023-02-24 23:45:44 +0530

    stripe commit

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,11 +1,25 @@
 import axios from 'axios';
+import { showAlert } from './alert';
 const stripe = Stripe(
   'pk_test_51Mf1iiSJYy9Yi6yXDT6It0sOaGz7b0JCUGCMMHudLVAumTC5ZpOSP6j2Pilr7rq4Br0ILNdbBfybjgA6VVcHe6Rh00SJqBkOp4'
 );
 
 export const bookTour = async (tourId) => {
-  const session = await axios(
-    `http://127.0.0.1/api/vi/booking/checkout-session/${tourId}`
-  );
-  console.log(session);
+  try {
+    //1) Get checkout session from api
+    const session = await axios(
+      `http://127.0.0.1:8000/api/v1/booking/chechkout-session/${tourId}`
+    );
+    console.log(session);
+
+    //2) Create checkout session form
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
 };
+
+// http://127.0.0.1:8000/api/v1/booking/chechkout-session/${tourId}
