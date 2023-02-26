8ad1fa3cb0cdeadc833db69278bdda5806d27dc9
1amSumit <sj779619@gmail.com>
2023-02-26 10:11:36 +0530

    stripe commit

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -2,6 +2,7 @@ const Tour = require('../models/toursModel');
 const factory = require('../controllers/factoryController');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+const Booking = require('../models/bookingModel');
 //It will give stripe object after we pass api key
 const stripe = require('stripe')(process.env.STRIPE_SECRET_API_KEY);
 
@@ -12,7 +13,9 @@ exports.getChechoutSession = catchAsync(async (req, res, next) => {
   //2 Create the checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -38,4 +41,14 @@ exports.getChechoutSession = catchAsync(async (req, res, next) => {
     status: 'success',
     session,
   });
+});
+
+exports.createCheckout = catchAsync(async (req, res, next) => {
+  const { tour, user, price } = req.query;
+  if (!tour || !user || !price) return next();
+
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split('?')[0]);
+  next();
 });
