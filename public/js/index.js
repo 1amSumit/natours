b6feb4b28607b0933e62515880f5828589e1c33d
1amSumit <sj779619@gmail.com>
2023-02-27 19:13:28 +0530

    stripe Improved implemented

--- public/js/index.js
+++ public/js/index.js
@@ -85,6 +85,6 @@ if (bookBtn) {
 
 const alertMessage = document.querySelector('body').dataset.alert;
 
-if (alert) {
+if (alertMessage) {
   showAlert('success', alertMessage, 20);
 }
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -9,12 +9,14 @@ export const bookTour = async (tourId) => {
   try {
     //1) Get checkout session from api
     const session = await axios(`/api/v1/booking/chechkout-session/${tourId}`);
+    console.log(session);
 
     //2) Create checkout session form
     await stripe.redirectToCheckout({
       sessionId: session.data.session.id,
     });
   } catch (err) {
-    showAlert('error', err);
+    console.log(err.message);
+    showAlert('error', err.message);
   }
 };
