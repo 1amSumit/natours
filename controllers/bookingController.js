aba46b8f98e288a09a4fc366b917635f86de431e
1amSumit <sj779619@gmail.com>
2023-02-27 11:34:01 +0530

    stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -46,7 +46,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  console.log(tour);
   const user = (await User.findOne({ email: session.customer_email }))._id;
   const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
