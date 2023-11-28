8bf94bef2036bb84bee1758a2fe6a3c06ef5bb94
Sumit kumar jha <85840676+1amSumit@users.noreply.github.com>
2023-11-28 21:07:15 +0530

    Update bookingModel.js

--- models/bookingModel.js
+++ models/bookingModel.js
@@ -27,7 +27,7 @@ const bookingSchema = new mongoose.Schema({
 
 bookingSchema.pre(/^find/, function (next) {
   this.populate('user').populate({
-    path: 'toue',
+    path: 'tour',
     select: 'name',
   });
   next();
