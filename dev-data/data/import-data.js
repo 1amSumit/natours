90390b765415c4fe2ac4d8af8fa7b7e2819f181e
1amSumit <sj779619@gmail.com>
2024-04-06 11:40:29 +0530

    some changes

--- dev-data/data/import-data.js
+++ dev-data/data/import-data.js
@@ -7,17 +7,12 @@ const Review = require('../../models/reviewModel');
 
 dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+const DB = process.env.DATABASE;
 
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
   })
   .then(() => {
     console.log('connected to Database successful');
